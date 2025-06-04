a09399769541accd6ab4af1187541f44edbf4c0c
Nipun Perera <nipun5perera@gmail.com>
2025-06-04 19:37:07 +1000

    chore: add shadcn button

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,12 @@
-export default function HomePage() {
-  return <>hello world</>
+import { Button } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+
+export default function HomePage()
+{
+  return (
+    <div className="container max-w-md">
+      <Textarea/>
+      <Button>Create</Button>
+    </div>
+  )
 }
